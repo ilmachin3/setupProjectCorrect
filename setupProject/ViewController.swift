49a4473398bc270b2a9fa71f11ed1b688c2f6819
ilmachin3 <iliayoyos@mail.ru>
2024-03-02 01:11:24 +0300

    Initial Commit

---
+++ setupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupProject
+//
+//  Created by Илья Дышлюк on 02.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
