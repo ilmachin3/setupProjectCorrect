0f8f0d1bfff49babeba07255020b7124897478d7
ilmachin3 <iliayoyos@mail.ru>
2024-04-15 00:52:55 +0300

    Готовый 9 спринт

---
+++ setupProject/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  setupProject
+//
+//  Created by Илья Дышлюк on 23.03.2024.
+//
+
+import Foundation
---
+++ setupProject/SingleImageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SingleImageViewController.swift
+//  setupProject
+//
+//  Created by Илья Дышлюк on 28.03.2024.
+//
+
+import Foundation
