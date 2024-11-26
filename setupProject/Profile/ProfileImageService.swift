a3ded3069b21dc89203bbcff914612d1e7fad925
ilmachine <iliayoyos@mail.ru>
2024-11-26 23:33:26 +0300

    финальные поправки

--- setupProject/Profile/ProfileImageService.swift
+++ setupProject/Profile/ProfileImageService.swift
@@ -38,7 +38,7 @@ final class ProfileImageService: ProfileImageServiceProtocol {
     }
     
     struct ProfileImageURL: Codable {
-        let small: String
+        let large: String
     }
     
     func fetchProfileImageURL(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
@@ -64,14 +64,14 @@ final class ProfileImageService: ProfileImageServiceProtocol {
         task = session.objectTask(for: request) { (result: Result<UserResult, Error>) in
             switch result {
             case.success(let userResult):
-                self.avatarURL = userResult.profileImage.small
+                self.avatarURL = userResult.profileImage.large
                 NotificationCenter.default
                     .post(
                         name: ProfileImageService.didChangeNotification,
                         object: self,
-                        userInfo: ["URL": userResult.profileImage.small]
+                        userInfo: ["URL": userResult.profileImage.large]
                     )
-                completion(.success(userResult.profileImage.small))
+                completion(.success(userResult.profileImage.large))
             case .failure(let error):
                 let invalidSessionError = ProfileImageServiceError.urlSessionError
                 print("[objectTask]: fetchProfileImageURL - \(invalidSessionError)")
